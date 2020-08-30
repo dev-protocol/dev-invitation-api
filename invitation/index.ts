1badbeb5590d21fd0030eff2aeb94f4231ffeddd
naoki-maeda <infrontofden@gmail.com>
2020-08-30 13:55:04 +0900

    add invite api

---
+++ invitation/index.ts
@@ -0,0 +1,54 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import { config } from 'dotenv'
+import { recover } from '../lib/recover'
+import { createData, AirTableField } from '../lib/airtable'
+
+// eslint-disable-next-line functional/no-expression-statement
+config()
+
+type Response = {
+	readonly status: number
+	readonly body: string | Record<string, unknown>
+	readonly headers?: {
+		readonly [key: string]: string
+	}
+}
+
+const invite: AzureFunction = async (
+	context: Context,
+	req: HttpRequest
+): Promise<Response> => {
+	const {
+		message = '',
+		signature = '',
+		market = '',
+		asset = '',
+		email = '',
+		discord = '',
+	} = req.body
+	const recoverAccount =
+		message && signature ? recover(message, signature) : undefined
+	const address = recoverAccount ? recoverAccount : ''
+	const fields: AirTableField = {
+		message,
+		signature,
+		market,
+		asset,
+		email,
+		discord,
+		address,
+	}
+
+	const apiKey = process.env.AIRTABLE_API_KEY as string
+	const endpoint = process.env.AIRTABLE_ENDPOINT as string
+	const success = await createData(apiKey, endpoint, fields)
+	const status = success ? 200 : 400
+	return {
+		status,
+		body: {
+			success: success,
+		},
+	}
+}
+
+export default invite
